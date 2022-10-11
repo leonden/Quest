ece961afad3d281ea647dd7592799d9c2394415f
leonh <leon.hochwimmer@breitband.ch>
2022-10-11 15:00:17 +0200

    remote bugfix

--- components/remote/src/remote/remote.ino
+++ components/remote/src/remote/remote.ino
@@ -1,18 +1,33 @@
-int pinPowerSwitch = 12;
-int statusPowerSwitch = 0;
-int pinIndicatorLed = 10;
+int ignitionSwitch = 12;
+int ledRed = 10;
+int ledGreen = 8;
+int readIgnitionSwitch = 0;
+int count = 0;
 
-void setup()
-{
-    pinMode(pinPowerSwitch, INPUT);
-    pinMode(pinIndicatorLed, OUTPUT);
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledGreen, OUTPUT);
+  pinMode(ledRed, OUTPUT);
+  pinMode(ignitionSwitch, INPUT_PULLUP);
 }
 
-void loop()
-{
-    statusPowerSwitch.digitalRead(pinPowerSwitch);
+void loop() {
 
-    if (statusPowerSwitch == HIGH) {
-      digitalWrite(pinIndicatorLed, HIGH);
+  digitalWrite(ledGreen, HIGH);
+  readIgnitionSwitch = digitalRead(ignitionSwitch);
+
+  if (readIgnitionSwitch == LOW && count == 0) {
+    for (int i = 1; i <= 10; i++) {
+      digitalWrite(ledRed, HIGH);
+      delay(500);
+      digitalWrite(ledRed, LOW);
+      delay(500);
+      if (readIgnitionSwitch == HIGH) {
+        digitalWrite(ledRed, LOW);
+      }
     }
+    count++;
+  } else if (readIgnitionSwitch == HIGH) {
+    count = 0;
+  }
 }
